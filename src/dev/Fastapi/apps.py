10a1f7d77eab873a30dcce94c7a08bdc0aa58565
Johnson Chong <johnsonhk88@gmail.com>
2025-02-05 23:57:12 +0800

    feat: add setmodel to vertix Ai model

--- src/dev/Fastapi/apps.py
+++ src/dev/Fastapi/apps.py
@@ -23,6 +23,7 @@ API_KEY = os.getenv("GOOGLE_API_KEY", default="")
 
 # set current used model
 usedModel = ""
+model = None
 
 
 def set_logging_level(level):
@@ -44,6 +45,21 @@ set_logging_level(logLevel)
 logger.info("PORT: " + PORT)
 # logger.info("Google API Key: " + API_KEY)
 
+class CFG(object):
+    Temperature = 0.5
+    TopP = 0.9
+    TopK = 50
+    MaxOutputTokens = 100
+    ResponseMimeType1 = "text/plain"
+
+generation_config = {    
+    "temperature": CFG.Temperature,
+    "top_p" : CFG.TopP,
+    "top_k" : CFG.TopK, 
+     "max_output_tokens" : CFG.MaxOutputTokens,
+    # "response_mime_type": CFG.ResponseMimeType1
+    
+}
 
 # initialize google api client
 genai.configure(api_key = API_KEY)
@@ -59,38 +75,59 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def initalizeModel(modelName):
+    """
+    initialize the model
+    """
+    model = genai.GenerativeModel(modelName, generation_config=generation_config)
+    return model
 
 
-def listModels():
+def getListModels():
     """
     get gemini current support models list
     """
     models = genai.list_models()
-    for model in models:
-        logger.info(model.name)
-    return models
+    listModels = [ model.name for model in models]
+    return listModels
 
 @app.get("/")
 async def first_api():
     return {"message": "Hello Johnson"}
 
-@app.post("setModels")
-async def set_models(model: SelectModel):
-    usedModel = model
+@app.post("/setModels")
+async def set_models(modelName: SelectModel):
+    global usedModel, model
+    usedModel = modelName.modelName
+    # initialize the model
+    model = initalizeModel(usedModel)
     return {"models": usedModel}
 
 @app.get("/getmodel")
 async def get_model():
+    global usedModel
     return {"model": usedModel}
 
+@app.post("/app/chat")
+async def chat(request: Request):
+    global model
+    response = model.chat(request.json())
+    return response
 
 @app.get("/getmodel-list")
 async def get_model_list():
-    models = genai.list_models()
-    listModels = [ model.name for model in models]
+    listModels = getListModels()
     logger.info("List of models: " + str(listModels))
     return {"models": f"{listModels}"}
 
+## HTTP exceptions
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    logger.error(f"HTTP error: {exc.status_code} - {exc.detail}")
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail},
+    )
 
 
 if __name__ == "__main__":
